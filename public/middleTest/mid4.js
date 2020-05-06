d98587e63b6f2aa47f14e23ba60d5dcb61d10ee5
hyeonji <hyeonji.27@daum.net>
2020-05-06 10:22:06 +0900

    middle test 4

--- public/middleTest/mid4.js
+++ public/middleTest/mid4.js
@@ -1,1 +1,22 @@
-let a = [];
+let num = [];
+
+for(let i=0; i<100; i++) {
+    num[i] = Math.floor(Math.random()*100 +1);
+    //console.log(num[i]);
+}
+
+function callback(a, b, i) {
+    if(i == 1) {
+        if(a%2 == 0) {
+            a = 1;
+        } else {
+            a = 0;
+        }
+    }
+    if(b%2 == 0) {
+        b = a+1;
+        return b;
+    }
+}
+
+console.log(num.reduce(callback));
