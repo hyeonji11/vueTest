d5dd21777abb1ec15c60a565967b8486d8828f59
hyeonji <hyeonji.27@daum.net>
2020-05-06 09:21:10 +0900

    middle test 1

---
+++ public/middleTest/mid1.js
@@ -0,0 +1,13 @@
+let num = [];
+let a = [5, 6, 7, 8, 9];
+
+for(let i=0; i<100; i++) {
+    num[i] = Math.floor(Math.random()*100 +1);
+    console.log(num[i]);
+}
+
+let num2 = num.filter((e) => {
+    for(let i=0; i<a.length; i++) {
+        
+    }
+});
