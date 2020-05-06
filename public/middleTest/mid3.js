a7248a316dbc632c1e8084c738b8f261b33ac19c
hyeonji <hyeonji.27@daum.net>
2020-05-06 10:00:43 +0900

    middle test 3

--- public/middleTest/mid3.js
+++ public/middleTest/mid3.js
@@ -1,5 +1,17 @@
 let a1 = [];
 
-for(let i=0; i<5; i += 2) {
-    
+for(let i=0; i<5; i=i+2) {
+    a1[i/2] = [i, i+1];
 }
+
+let a2 = [];
+
+let clone = (e) => {
+    a2.push(e.slice(0));
+    return true;
+}
+a1.every(clone);
+
+a1[0][0] = 100;
+console.log(a1);
+console.log(a2);
